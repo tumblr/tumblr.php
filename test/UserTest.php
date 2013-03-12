b14fb6bc20d7abe7d5a394cba2705458ed41d137
John Crepezzi <john.crepezzi@gmail.com>
2013-03-12 12:44:48 -0400

    Fix names in test comments

--- test/UserTest.php
+++ test/UserTest.php
@@ -5,18 +5,18 @@ class UserTest extends TumblrTest {
     public function providerCalls() {
         return array(
 
-            // getInfo
+            // getUserInfo
             array(function ($c) { $c->getUserInfo(); }, 'GET', 'v2/user/info', null),
 
-            // getDashboard
+            // getDashboardPosts
             array(function ($c) { $c->getDashboardPosts(); }, 'GET', 'v2/user/dashboard', null),
             array(function ($c) { $c->getDashboardPosts(array('limit' => 10)); }, 'GET', 'v2/user/dashboard', array('limit' => 10)),
 
-            // getFollowing
+            // getFollowedBlogs
             array(function ($c) { $c->getFollowedBlogs(); }, 'GET', 'v2/user/following', null),
             array(function ($c) { $c->getFollowedBlogs(array('limit' => 10)); }, 'GET', 'v2/user/following', array('limit' => 10)),
 
-            // getFollowing
+            // getLikedPosts
             array(function ($c) { $c->getLikedPosts(); }, 'GET', 'v2/user/likes', null),
             array(function ($c) { $c->getLikedPosts(array('limit' => 10)); }, 'GET', 'v2/user/likes', array('limit' => 10)),
 
