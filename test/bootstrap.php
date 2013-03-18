18c25aa61e146ba896198362660854028d5832c8
John Crepezzi <john.crepezzi@gmail.com>
2013-03-18 10:56:04 -0400

    Use composer autoloader in test bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,7 +3,6 @@
 date_default_timezone_set('America/New_York');
 
 require_once 'vendor/autoload.php';
-require_once 'lib/Tumblr/API/Client.php';
 require_once 'test/TumblrTest.php';
 
 define('API_KEY', 'the testing consumer key');
